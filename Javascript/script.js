a4c28c0cb193784ede59e179725219f6b1bff15a
Bobby P <abpupeerapitug@yahoo.com>
2017-05-22 20:53:11 -0500

    Complete the first tutorial.

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,4 +1,10 @@
 /**
  * Created by abpxninja on 5/22/2017.
  */
-console.log("From the script file");
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+var fullName = firstName + " " + lastName;
+
+console.log("Your full name is " + fullName + ".");
+console.log("You are " + age + " years old.");
