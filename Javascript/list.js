64a62c2b137eac621b90eb5931a33fd91c140745
Bobby P <abpupeerapitug@yahoo.com>
2017-05-25 22:48:24 -0500

    Added while loop

--- Javascript/list.js
+++ Javascript/list.js
@@ -2,14 +2,20 @@ var todos = ["Buy New Item"];
 
 var input = prompt("What would you like to do?");
 
-if(input === "list") {
-    console.log(todos);
-}
-else if(input === "new") {
-    // Ask for new todo
+while(input !== "quit") {
+    // handle new input
+    if(input === "list") {
+        console.log(todos);
+    }
+    else if(input === "new") {
+        // Ask for new todo
+        var newTodo = prompt("Enter new todo");
 
-    var newTodo = prompt("Enter new todo");
-    // add to todos array
-    todos.push(newTodo);
+        // add to todos array
+        todos.push(newTodo);
+    }
 
+    // ask  again for new input
+    input = prompt("What would you like to do?");
 }
+console.log("Ok, you quit the app");
