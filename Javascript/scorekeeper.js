0600d847f269dc53bf5a3183a9450bfb49eb0405
Bobby P <abpupeerapitug@yahoo.com>
2017-05-29 10:47:36 -0500

    Modified the scorekeeper to turn green on 5.

--- Javascript/scorekeeper.js
+++ Javascript/scorekeeper.js
@@ -11,6 +11,7 @@ p1Button.addEventListener("click", function() {
     if(!gameOver) {
         p1Score++;
         if(p1Score === winningScore) {
+            p1Display.classList.add("winner");
             gameOver = true;
         }
         p1Display.textContent = p1Score;
@@ -21,8 +22,16 @@ p2Button.addEventListener("click", function() {
     if(!gameOver) {
         p2Score++;
         if(p2Score === winningScore) {
+            p2Display.classList.add("winner");
             gameOver = true;
         }
         p2Display.textContent = p2Score;
     }
+});
+
+resetButton.addEventListener("click", function() {
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
 });
