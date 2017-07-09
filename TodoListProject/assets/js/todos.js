75be960f6341464e8e331d077af4d6e9d8d1f7ac
Bobby P <abpupeerapitug@yahoo.com>
2017-07-09 14:06:48 -0500

    Replace the jQuery file from slim to min. Also added delete function from todo list.

--- TodoListProject/assets/js/todos.js
+++ TodoListProject/assets/js/todos.js
@@ -1,3 +1,16 @@
 /**
  * Created by abpxninja on 7/8/2017.
  */
+// Check off specific todos by clicking
+$("li").click(function () {
+    $(this).toggleClass("completed");
+});
+
+// Click on X to delete to-do
+$("span").click(function(event){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
