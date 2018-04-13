e7ff34a4f1aff7b593b76b6938fd4454d8a12510
Bobby P <abpupeerapitug@yahoo.com>
2018-04-13 07:47:17 -0500

    Adding new file

---
+++ youtube/Login/Basic/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abpxninja
+ * Date: 4/13/2018
+ * Time: 7:42 AM
+ */
+
+$users = array(
+    array(
+        'username'  => 'test',
+        'password'  => 'test',
+        'name'      => 'My Name',
+    ),
+);
